ec12867857e911c3b07c9fcf52c6adb4199061d3
xianshianwen <42104032+xianshianwen@users.noreply.github.com>
2019-08-31 16:51:23 +0800

    第一次提交

---
+++ app/src/main/java/com/example/theverysmartclass/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.theverysmartclass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
