6d0b604ec0a663462428a254308f59610b82a5eb
kjt <1663852002@qq.com>
2019-08-31 18:29:09 +0800

    kjt第一次提交

--- app/src/main/java/com/example/theverysmartclass/MainActivity.java
+++ app/src/main/java/com/example/theverysmartclass/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.theverysmartclass;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.theverysmartclass.personal_center.PersonalActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button Loginbtn=(Button)findViewById(R.id.Loginbtn);
+        Loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, PersonalActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/theverysmartclass/personal_center/PersonalActivity.java
@@ -0,0 +1,15 @@
+package com.example.theverysmartclass.personal_center;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.theverysmartclass.R;
+
+public class PersonalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal);
+    }
+}
